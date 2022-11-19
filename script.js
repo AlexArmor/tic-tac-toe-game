004523426cef33d9debb5b7d85500004cb7d7e85
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-19 18:54:53 +0200

    2th commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cellArray = new Array(9);
 const gameField = document.querySelector('.field');
+let playerX = true;
 
 for (let i = 0; i < 9; i += 1) {
     const cell = document.createElement('div');
@@ -11,5 +12,7 @@ gameField.addEventListener('click', onGameFieldClick);
 
 function onGameFieldClick(event) {
     const index = [...gameField.children].indexOf(event.target);
-    console.dir(index);
+    if (playerX) {
+        event.target.textContent = 'X';
+    }
 };
