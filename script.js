a17345d9a2db6440449e16e795bf5c4b0c0ba275
Oleksandr Bron <lexlutermotion@gmail.com>
2022-11-19 19:29:34 +0200

    2th commit

--- script.js
+++ script.js
@@ -4,15 +4,30 @@ let playerX = true;
 
 for (let i = 0; i < 9; i += 1) {
     const cell = document.createElement('div');
-    cell.classList.add('cell');
+    cell.classList.add('cell', 'kuk');
     gameField.append(cell);
 }
 
 gameField.addEventListener('click', onGameFieldClick);
 
-function onGameFieldClick(event) {
-    const index = [...gameField.children].indexOf(event.target);
-    if (playerX) {
-        event.target.textContent = 'X';
+function onGameFieldClick({ target }) {
+    const index = [...gameField.children].indexOf(target);
+    if (!target.classList.value.includes('cell')) {
+        return;
     }
+    if (target.textContent) {
+        return
+    };
+    target.textContent = playerX ? 'X' : 'O';
+
+    playerX = !playerX;
+    cellArray[index] = target.textContent;
+
+    isGameOver();
 };
+
+function isGameOver() {
+    if (cellArray[0] === cellArray[1] && cellArray[0] === cellArray[2] || cellArray[0] === cellArray[3] && cellArray[0] === cellArray[6] || cellArray[0] === cellArray[3] && cellArray[0] === cellArray[6]) {
+        console.log('Win');
+    }
+}
